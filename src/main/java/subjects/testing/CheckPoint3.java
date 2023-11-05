41abea9ee68d0c6da3e1353ba93272866fea78ac
Asya Blaze <anastasiaalek67@yandex.ru>
2023-11-05 17:31:50 +0300

    3 КТ

--- src/main/java/subjects/testing/CheckPoint3.java
+++ src/main/java/subjects/testing/CheckPoint3.java
@@ -1,169 +1,158 @@
 package subjects.testing;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.json.simple.JSONObject;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
+import java.util.Arrays;
+import java.util.Date;
 
 public class CheckPoint3 {
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("\nДобавляем пользователя...");
+        String url = "/user";
+        User user = new User(3, "kiko", "Janin", "Kirova", "kiko567@numa.com", "lipetsk1997");
+        makePostOrPutRequest(url, new ObjectMapper().writeValueAsString(user), "POST");
 
+        System.out.println("\nПроверяем добавление пользователя...");
+        url = "/user/kiko";
+        makeGetRequest(url);
 
-        //POST-запрос питомца
-        String url = "/pet/12";
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(new Pet(12, "Jessy"));
-        makePostRequest(url, json);
+        System.out.println("\nОбновляем пароль...");
+        user.setPassword("KiUlOp02");
+        makePostOrPutRequest(url, new ObjectMapper().writeValueAsString(user), "PUT");
 
-        // GET-запрос для нахождения питомца с id 1
-        url = "/pet/12";
-        java.net.http.HttpResponse<String> response1 = makeGetRequest(url);
-        System.out.println(response1.body());
+        System.out.println("\nПроверяем обновленный пароль...");
+        makeGetRequest(url);
+
+        System.out.println("\nУдаляем пользователя...");
+        makeDeleteRequest(url);
+
+        System.out.println("\nПроверяем удалился ли пользователь...");
+        makeGetRequest(url);
+
+        System.out.println("\n\n----Работа с заказами----");
+        System.out.println("\nДобавляем заказ...");
+        url = "/store/order";
+        Order order = new Order(5, 21, 1, "2023-11-05T14:25:18.747Z", Status.approved, false);
+        makePostOrPutRequest(url, new ObjectMapper().writeValueAsString(order), "POST");
+
+        System.out.println("\nПроверяем добавление заказа...");
+        url = "/store/order/5";
+        makeGetRequest(url);
+
+        System.out.println("\nУдаляем заказ");
+        makeDeleteRequest(url);
+
+        System.out.println("\nПроверяем удалился ли заказ...");
+        makeGetRequest(url);
+
+        System.out.println("\nДелаем инвентаризацию...");
+        url = "/store/inventory";
+        makeGetRequest(url);
     }
 
-    private static java.net.http.HttpResponse<String> makeGetRequest(String url) throws IOException, InterruptedException {
-        String site = "https://petstore.swagger.io/v2";
-        java.net.http.HttpClient client = java.net.http.HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(site + url))
-                .GET()
-                .build();
-        return client.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
-    }
-
-    private static void makePostRequest(String url, String body) throws IOException, InterruptedException {
+    private static void makeDeleteRequest(String url) throws IOException {
         URL uri = new URL("https://petstore.swagger.io/v2" + url);
         HttpURLConnection connection = (HttpURLConnection) uri.openConnection();
-        connection.setRequestMethod("POST");
-//        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestMethod("DELETE");
+        connection.setRequestProperty("accept", "application/json");
+        connection.connect();
+        System.out.println("Delete Response Code for " + url + ": " + connection.getResponseCode());
+    }
+
+    private static void makeGetRequest(String url) throws IOException, InterruptedException {
+        String site = "https://petstore.swagger.io/v2" + url;
+        java.net.http.HttpClient client = java.net.http.HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(site))
+                .GET()
+                .build();
+        System.out.print("Response for GET-request " + url + ": ");
+        System.out.println(client.send(request, java.net.http.HttpResponse.BodyHandlers.ofString()).body());
+    }
+
+    private static void makePostOrPutRequest(String url, String body, String requestType) throws IOException, InterruptedException {
+        URL uri = new URL("https://petstore.swagger.io/v2" + url);
+        HttpURLConnection connection = (HttpURLConnection) uri.openConnection();
+        connection.setRequestMethod(requestType);
+        connection.setRequestProperty("accept", "application/json");
+        connection.setRequestProperty("Content-Type", "application/json");
         connection.setDoOutput(true);
         try (OutputStream os = connection.getOutputStream()) {
             byte[] input = body.getBytes("UTF-8");
             os.write(input,0, input.length);
         }
-        int responseCode = connection.getResponseCode();
-        System.out.println("Response Code: " + responseCode);
-    }
-}
-
-class Category {
-    private int id = 0;
-    private String name = "sting";
-
-    public Category(int id, String name) {
-        this.id = id;
-        this.name = name;
+        System.out.println("Response Code for " + requestType +"-request " + url + ": " + connection.getResponseCode());
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
-
-class Tag {
-    private int id = 0;
-    private String name = "sting";
-
-    public Tag(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
 enum Status {
-    available,
-    pending,
-    sold
+    placed,
+    approved,
+    delivered
 }
 
-class Pet {
-    private int id;
-    private Category category;
-    private String name;
-    private String[] photoUrls;
-    private Tag[] tags;
+class Order {
+    private long id;
+    private long petId;
+    private int quantity;
+    private String shipDate;
     private Status status;
+    private boolean complete;
 
-    public Pet(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public Order() {
     }
 
+    public Order(long id, long petId, int quantity, String shipDate, Status status, boolean complete) {
+        this.id = id;
+        this.petId = petId;
+        this.quantity = quantity;
+        this.shipDate = shipDate;
+        this.status = status;
+        this.complete = complete;
+    }
 
-
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
-    public Category getCategory() {
-        return category;
+    public long getPetId() {
+        return petId;
     }
 
-    public void setCategory(Category category) {
-        this.category = category;
+    public void setPetId(long petId) {
+        this.petId = petId;
     }
 
-    public String getName() {
-        return name;
+    public int getQuantity() {
+        return quantity;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
-    public String[] getPhotoUrls() {
-        return photoUrls;
+    public String getShipDate() {
+        return shipDate;
     }
 
-    public void setPhotoUrls(String[] photoUrls) {
-        this.photoUrls = photoUrls;
-    }
-
-    public Tag[] getTags() {
-        return tags;
-    }
-
-    public void setTags(Tag[] tags) {
-        this.tags = tags;
+    public void setShipDate(String shipDate) {
+        this.shipDate = shipDate;
     }
 
     public Status getStatus() {
@@ -172,5 +161,111 @@ class Pet {
 
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
+
+class User {
+    private long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private int userStatus;
+
+    public User() {
+    }
+
+    public User(long id, String username, String firstName, String lastName, String email, String password, String phone, int userStatus) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
+
+    public User(long id, String username, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(int userStatus) {
+        this.userStatus = userStatus;
     }
 }
